a5a486b00c8943fe52b5d2c00edde55e03b531a9
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2025-10-08 23:25:52 +0700

    sync for watcher

---
+++ internal/generator/sync.go
@@ -0,0 +1,42 @@
+package generator
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/alberdjuniawan/anstruct/internal/core"
+)
+
+// collectAllowed: kumpulkan semua path relatif yang valid dari blueprint
+func CollectAllowed(n *core.Node, prefix string, allowed map[string]bool) {
+	var path string
+	if prefix == "" {
+		path = n.Name
+	} else {
+		path = filepath.Join(prefix, n.Name)
+	}
+	allowed[path] = true
+	for _, c := range n.Children {
+		CollectAllowed(c, path, allowed)
+	}
+}
+
+// cleanupExtra: hapus file/folder yang tidak ada di blueprint
+func CleanupExtra(outputDir string, allowed map[string]bool) error {
+	return filepath.WalkDir(outputDir, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == outputDir {
+			return nil
+		}
+		rel, _ := filepath.Rel(outputDir, path)
+		if !allowed[rel] {
+			if d.IsDir() {
+				return os.RemoveAll(path)
+			}
+			return os.Remove(path)
+		}
+		return nil
+	})
+}
